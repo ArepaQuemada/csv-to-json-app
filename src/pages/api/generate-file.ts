385614855ee26a787c24d9122e18074042a7fb8d
Juan <rigualjuan@gmail.com>
2023-03-09 19:22:56 -0300

    restore

--- src/pages/api/generate-file.ts
+++ src/pages/api/generate-file.ts
@@ -1,7 +1,12 @@
 import { readFileData } from "../../utilities/handle-stream";
 
 export const post = async ({ request }) => {
+  console.log("LOGGING SERVERLESS FN ");
+  const data = await request.formData();
+  const blobFile = data.get("file") as Blob;
+  const streamReader = blobFile.stream().getReader();
+  const jsonData = await readFileData(streamReader);
   return {
-    body: JSON.stringify([{ example: "" }]),
+    body: jsonData,
   };
 };
