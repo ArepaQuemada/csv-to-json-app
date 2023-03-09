7f158f2d035ad40a87efe57ea94250ec319c4126
Juan <rigualjuan@gmail.com>
2023-03-09 19:02:57 -0300

    mock return

--- src/pages/api/generate-file.ts
+++ src/pages/api/generate-file.ts
@@ -1,12 +1,7 @@
 import { readFileData } from "../../utilities/handle-stream";
 
 export const post = async ({ request }) => {
-  console.log("LOGGING SERVERLESS FN ");
-  const data = await request.formData();
-  const blobFile = data.get("file") as Blob;
-  const streamReader = blobFile.stream().getReader();
-  const jsonData = await readFileData(streamReader);
   return {
-    body: jsonData,
+    body: JSON.stringify([{ example: "" }]),
   };
 };
